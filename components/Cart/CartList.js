861bf080cec58f3562bab959014a057806db5b0f
Ghadahb <ghada.dxb@gmail.com>
2021-11-21 15:32:43 +0300

    task 5- cart item delete and checkout

--- components/Cart/CartList.js
+++ components/Cart/CartList.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import CartItem from './CartItem';
-import cartStore from '../../stores/cartStore';
-import { Center, VStack } from  'native-base';
-import { observer } from 'mobx-react';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CartItem from "./CartItem";
+import cartStore from "../../stores/cartStore";
+import { Center, VStack } from "native-base";
+import { observer } from "mobx-react";
 
 const CartList = () => {
-    const cartList = cartStore.items.map((item) => (
-        <CartItem key={item.product._id} item={item} />
-    ));
-    return (
-        <View>
-            <VStack space="6">{cartList}</VStack>
-            <Button onPress={cartStore.checkout}>Checkout</Button>
-        </View>
-       
-    );
+  const cartList = cartStore.items.map((item) => (
+    <CartItem key={item.product._id} item={item} />
+  ));
+  return (
+    <View>
+      <VStack space="6">{cartList}</VStack>
+      <Button onPress={cartStore.checkout}>Checkout</Button>
+    </View>
+  );
 };
-
 
 export default observer(CartList);
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
